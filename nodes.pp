42a022d01d36b6cf441efa0acf08dc2ca5e33ada
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2011-12-20 22:06:29 -0500

    move files package config into nodes.pp

--- nodes.pp
+++ nodes.pp
@@ -2,11 +2,30 @@ node default {
   include basehost
 }
 
-node 'files' {
+node 'files.house' inherits default {
   include libvirt
   include apache
   include samba
 
-  include node-files
+  $packages = [
+    qemu-kvm,
+    python-virtinst,
+    rpm-build,
+    rpm-sign,
+    ruby-rdoc,
+    python-docutils,
+    python-virtualenv,
+    python-setuptools,
+    python-lxml,
+    rlwrap,
+    mock,
+    nmap,
+    libxml2,
+    libxslt,
+    python-ldap,
+    virt-top,
+  ]
+
+  package { $packages: ensure => installed }
 }
 
