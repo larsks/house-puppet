4be588e61990d68b711aa0b6f9f633ddf3c04c8e
Lars Kellogg-Stedman <lars@oddbit.com>
2011-12-21 10:56:01 -0500

    install krb5 acl

--- modules/krb5-server/manifests/config.pp
+++ modules/krb5-server/manifests/config.pp
@@ -1,3 +1,11 @@
 class krb5-server::config {
 	require krb5-server::packages
+
+	file { '/var/kerberos/krb5kdc/kadm5.acl':
+		source => 'puppet:///modules/krb5-server/kadm5.acl',
+		owner => root,
+		group => root,
+		mode => 0600,
+	}
 }
+
